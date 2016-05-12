14ecb5fb989109e327b63290597aa52aed5c568a
Robert Einsle <robert@einsle.de>
2016-05-12 21:49:27 +0200

    Update code, set defaults, read config file

--- virtcreate/config.py
+++ virtcreate/config.py
@@ -1,22 +1,13 @@
-import configparser
 import os
 
-configfile = os.path.expanduser('/etc/virt-create/virt-create.conf')
+import configparser
+
+configfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '../virt-create.conf'))
 config = configparser.ConfigParser()
-config['virt_machine'] = {
-    'lv_prefix': 'vm_',
-    'vm_network': 'network=10-server,model=virtio',
-    'autoinst_url': 'http://www.einsle.de/wheezy-test.cfg',
-    'vm_domain': 'einsle.de',
-    'vm_keymap': 'de',
-    'vm_default_cpu_count': '1',
-    'vm_default_cpu_ram': '256',
-    'installer_location': 'http://ftp.de.debian.org/debian/dists/stable/main/installer-amd64'}
-config['testerle'] = {}
 
 
-def readconfig():
-    config.read([configfile, ])
+def readconfig(filename):
+    config.read([filename, configfile])
 
 
 def writeconfig(conf=config):
@@ -25,6 +16,3 @@ def writeconfig(conf=config):
         os.makedirs(configdir, exist_ok=True)
     with open(configfile, 'w') as f:
         conf.write(f)
-
-
-readconfig()
--- virtcreate/main.py
+++ virtcreate/main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
 import argparse
-import os
+
 import virtcreate.config as config
-from sh import virt_install
 
 
 class VirtCreate:
@@ -14,64 +13,75 @@ class VirtCreate:
         :return: parser instance
         """
         parser = argparse.ArgumentParser(description='Create virtual Machines')
-        parser.add_argument('-n', dest='name', type=str, help='name of vm', required=True)
-        parser.add_argument('-s', dest='size', type=int, default=15,
-                   help='size of created vm in GB')
-        parser.add_argument('-c', dest='cpu', type=int, default=1,
-                   help='numbers of cpus for vm')
-        parser.add_argument('-r', dest='ram', type=int, default=512,
-                   help='size of ram')
         parser.add_argument('-v', help='do verbose output', action='store_true')
+        parser.add_argument('-c', dest='config', type=str, default='/etc/virt-create.conf',
+                            help='path to virt-create configfile')
+        parser.add_argument('-n', dest='hostname', type=str, help='name of vm', required=True)
+        parser.add_argument('-d', dest='domainname', type=str, help='domainname of newly created host')
+        parser.add_argument('-s', dest='vm_size', type=str, help='size of created vm in GB')
+        parser.add_argument('--ram', dest='ram', type=str, help='amount of ram for the new vm')
+        parser.add_argument('--vcpu', dest='vcpu', type=str, help='count of virtual cpus')
         return parser
 
-    def createCommand(self, args):
+    def create_command(self, args):
         '''
         Create command to create virtual machine
 
         :param args: the programm arguments
         :return: the command to run
         '''
-        #virt_install.bake('--{}'.format(argname), '')
-        for key, value in config.config['virt_machine'].iteritems():
-            print(key, value)
+        # virt_install.bake('--{}'.format(argname), '')
+        # for key, value in config.config['virt_machine'].iteritems():
+        #    print(key, value)
 
 
-        print(virt_install)
+        # print(virt_install)
 
 
-        cmd = '''/usr/bin/virt-install \
-                --connect=qemu:///system \
-                --name={0} \
-                --ram={3} \
-                --vcpus={2} \
-                --os-type=linux \
-                --os-variant=debianwheezy \
-                --disk /dev/vg_data/lv_{0},bus=virtio,size={1} \
-                --network='network=10-server,model=virtio' \
-                --hvm \
-                --vnc \
-                --noautoconsole \
-                --keymap=de \
-                --location http://ftp.de.debian.org/debian/dists/stable/main/installer-amd64 \
-                --extra-args="auto=true hostname={0} domain=einsle.de url=http://www.einsle.de/wheezy-test.cfg"'''
-        cmd = cmd.format(args.name.lower(), args.size, args.cpu, args.ram)
-        return cmd
+        # cmd = '''/usr/bin/virt-install \
+        #        --connect=qemu:///system \
+        #        --name={0} \
+        #        --ram={3} \
+        #        --vcpus={2} \
+        #        --os-type=linux \
+        #        --os-variant=debianwheezy \
+        #        --disk /dev/vg_data/lv_{0},bus=virtio,size={1} \
+        #        --network='network=10-server,model=virtio' \
+        #        --hvm \
+        #        --vnc \
+        #        --noautoconsole \
+        #        --keymap=de \
+        #        --location http://ftp.de.debian.org/debian/dists/stable/main/installer-amd64 \
+        #        --extra-args="auto=true hostname={0} domain=einsle.de url=http://www.einsle.de/wheezy-test.cfg"'''
+        # cmd = cmd.format(args.name.lower(), args.size, args.cpu, args.ram)
+        # return cmd
+
+    def parse_arguments(self, args):
+        config.readconfig(args.config)
+        cfg = dict()
+        cfg['domainname'] = args.domainname if args.domainname else config.config['config']['domainname']
+        cfg['vm_size'] = args.vm_size if args.vm_size else config.config['config']['vm_size']
+        cfg['ram'] = args.ram if args.ram else config.config['config']['ram']
+        cfg['vcpu'] = args.vcpu if args.vcpu else config.config['config']['vcpu']
+        return cfg
 
     def main(self):
         parser = self.createArgumentParser()
         args = parser.parse_args()
-        cmd = self.createCommand(args)
+        # cmd = self.createCommand(args)
+        cfg = self.parse_arguments(args)
         if args.v:
-            print("Name: {0}".format(args.name.lower()))
-            print("Size: {0}".format(args.size))
-            print("Cpu:  {0}".format(args.cpu))
-            print("Ram:  {0}".format(args.ram))
-            print("Cmd:  {0}".format(cmd))
-        os.system(cmd)
+            print("Configpath:   {0}".format(args.config))
+            print("Hostname:     {0}".format(args.hostname.lower()))
+            print("Domainname:   {0}".format(cfg['domainname']))
+            print("VM Size:      {0} GB".format(cfg['vm_size']))
+            print("Ram:          {0}".format(cfg['ram']))
+            print("vCPU Count:   {0}".format(cfg['vcpu']))
 
 
 def entry_point():
     VirtCreate().main()
+
 
 if __name__ == "__main__":
     entry_point()
